f61f28d27fe050e71fd3d30403ac694501939cfa
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-08-11 04:02:37 -0400

    Read Me

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -1,89 +1,66 @@
-#Dependencies
+
 import os 
 import csv
+#path to the csvfile
+csvpath = os.path.join('election_data.csv')
 
-#Variable for the data set 
+#initializing the variables 
+poll_data={}
 total_votes = 0
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0  
+with open(csvpath, 'r') as csvfile:
+    csvread = csv.reader(csvfile)
+    next(csvread, None)
 
-# Set path for the file 
-csvpath = os.path.join(',','PyPoll','Resources','eletion_data.csv')
-
-# Open and Read csv file 
-with open(csvpath, newline='') as csvfile:
-    # Set delimiter and variables 
-    csvreader = csv.reader(csvfile, delimiter=',') 
-    
-    # Read the header of the row 
-    csv_header = next(csvfile)
-
-    # Set a Loop 
-    for row in csvreader: 
-
-        # to calculate total number of votes per candidate 
-        total_votes += 1 
-
-        # Calculate total number of votes each candidate won
-
-        if (row[2] == "Khan"):
-            khan_votes += 1 
-        elif (row[2] == "Correy"):
-            correy_votes += 1
-        elif (row [2] == "Li"):
-            li_votes += 1
+    for row in csvread:
+        total_votes += 1
+        if row[2] in poll_data.keys():
+            poll_data[row[2]] = poll_data[row[2]] + 1
         else:
-            otooley_votes += 1
-        
-    # Calculate the percentage of votes for each candidate 
-    khan_percent = khan_votes/ total_votes
-    correy_percent = correy_votes / total_votes
-    li_percent = li_votes / total_votes
-    otooley_percent = otooley_votes / total_votes
+            poll_data[row[2]] = 1 
+    
 
-    # Calculate winner of the election based on the maximum nuber of votes the candidate gets
+candidates = []  
+tot_num_votes = []
+# Total Number of votes
+for key, value in poll_data.items():
+    candidates.append(key)
+    tot_num_votes.append(value)
+  
+# Percentage of votes
+percentage_votes =[]
+for n in tot_num_votes:
+    percentage_votes.append(round(n/total_votes * 100, 1))
+ 
+# Finding the winner
+clean_data = list(zip(candidates, tot_num_votes, percentage_votes))
 
-    winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+winner_list = []
+for name in clean_data:
+    if max(tot_num_votes) == name[1]:
+        winner_list.append(name[0])
+winner = winner_list[0]
 
-    if winner == khan_votes:
-        winner_name = "Khan"
-    elif winner == correy_votes:
-        winner_name = "Correy"
-    elif winner == li_votes:
-        winner_name = "Li"
-    else:
-        winner_name = "Otooley" 
-# Print Analysis
-print(f"Election Results")
-print(f"---------------------------")
-print(f"Total Votes: {total_votes}")
-print(f"---------------------------")
-print(f"Khan: {khan_percent}({khan_votes})")
-print(f"Correy: {correy_percent}({correy_votes})")
-print(f"Li: {li_percent}({li_votes})")
-print(f"O'Tooley: {otooley_percent}({otooley_votes})")
-print(f"---------------------------")
-print(f"Winner: {winner_name}")
-print(f"---------------------------")
+# Print all data
+print ("Election results :")
+print(total_votes) 
+print(candidates)  
+print(percentage_votes)
+print(tot_num_votes)  
+print(winner)
+
+# Writng output files
+PyPoll = open("output.txt","w+")
+PyPoll.write("Election Results")  
+PyPoll.write('\n' + "Total_votes" + str(total_votes)) 
+PyPoll.write('\n' + str(candidates))
+PyPoll.write('\n' + str(percentage_votes))
+PyPoll.write('\n' + str(tot_num_votes)) 
+PyPoll.write('\n' + "Winner:" + winner)    
 
 
-# Write File
-output_file = os.path.join('.', 'PyPoll', 'Resources', 'election_data_revised.text')
 
-# Open file with 'w'
-with open(output_file, 'w',) as txtfile:
 
-# Write New Data
-    txtfile.write(f"Election Results")
-    txtfile.write(f"---------------------------")
-    txtfile.write(f"Total Votes: {total_votes}")
-    txtfile.write(f"---------------------------")
-    txtfile.write(f"Khan: {khan_percent}({khan_votes}")
-    txtfile.write(f"Correy: {correy_percent}({correy_votes})")
-    txtfile.write(f"Li: {li_percent}({li_votes})")
-    txtfile.write(f"O'Tooley: {otooley_percent}({otooley_votes})")
-    txtfile.write(f"---------------------------")
-    txtfile.write(f"Winner: {winner_name}")
-    txtfile.write(f"---------------------------")
+ 
+
+       
+            
