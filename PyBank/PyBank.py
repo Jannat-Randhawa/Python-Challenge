5f5c3951da69ac5d68e4aa7ba68043a8ded9d1c0
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-08-11 03:13:10 -0400

    loop code

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -12,9 +12,26 @@ greatest_increase_month = 0
 greatest_decrease = 0
 greatest_decrease_month = 0
 
-# Open the CSV
-with open(csvpath, newline = '') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    next(csvreader, None)
-    row = next(csvreader)
+# Set Path For File
+csvpath = os.path.join('.', 'PyBank', 'Resources', 'budget_data.csv')
+
+# Open & Read CSV File
+with open(csvpath, newline='') as csvfile:
     
+    # CSV Reader Specifies Delimiter & Variable That Holds Contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    # Read header row
+    # Set header row to skip to next row 
+    csv_header = next(csvreader)
+    row = next(csvreader)
+
+    #Set variables + Calculate total months + Net amount  
+    previous_row = int(row[1])
+    total_months += 1 
+    net_amount += int(row[1])
+    greatest_increase = int(row[1])
+    greatest_increase_month = row[0]
+
+    
+
