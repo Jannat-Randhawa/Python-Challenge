9fe998260f7c173dd06b048d6de7c0df4867ecd7
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-08-11 03:12:13 -0400

    code

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -11,3 +11,10 @@ greatest_increase = 0
 greatest_increase_month = 0
 greatest_decrease = 0
 greatest_decrease_month = 0
+
+# Open the CSV
+with open(csvpath, newline = '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    next(csvreader, None)
+    row = next(csvreader)
+    
