79fbd077ff0489724f80b7b81147acf294c77c48
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-08-11 03:19:15 -0400

    code

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -47,7 +47,7 @@ with open(csvpath, newline='') as csvfile:
         previous_profit = int(row[1])
         month_count.append(row[0])
 
-        #Calulate greatest increase 
+        #Calulate Greatest Increase 
         if int(row[1]) > greatest_increase:
             greatest_increase = int(row[1])
             greatest_increase_month = row[0]
@@ -56,6 +56,21 @@ with open(csvpath, newline='') as csvfile:
         if int(row[1]) < greatest_decrease:
             greatest_decrease = int(row[1])
             greatest_decrease_month = row[0]  
+      
+        # Calculate The Average & The Date
+        average_change = sum(monthly_change)/ len(monthly_change)
+
+        highest = max(monthly_change)
+        lowest = min(monthly_change)
+
+        print(f"Financial Analysis")
+        print(f"---------------------------")
+        print(f"Total Months: {total_months}")
+        print(f"Total: ${net_amount}")
+        print(f"Average Change: ${average_change:.2f}")
+        print(f"Greatest Increase in Profits:, {greatest_increase_month}, (${highest})")
+        print(f"Greatest Decrease in Profits:, {greatest_decrease_month}, (${lowest})")
+
 
 
 
